a959247e584746469508ae4080976220faddb629
Himanshu Bhardwa <gamingspray38@gmail.com>
2026-03-24 08:55:25 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+
+
+app = FastAPI()
+
+# cors fix
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods =["*"],
+    allow_headers = ["*"]
+)
+
+with open("pipeline.pkl",'rb') as f:
+    pipeline = pickle.load(f)
+
+class text_data(BaseModel):
+    text :str
+
+# home route
+@app.get("/")
+def home():
+    return {
+        'api' :'run succesfully '
+    }
+
+# predction api
+@app.post("/prediction")
+def predict(data : text_data):
+    result = pipeline.predict([data.text])
+
+    return{
+        'input':data.text,
+        'prediction':result[0]
+    }
